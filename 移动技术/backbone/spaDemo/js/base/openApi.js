6aa6a66133b1e94d6d7ed598784ee6271fac42ef
haven <641136947@qq.com>
2014-09-03 15:39:59 +0800

    api and 图片延迟加载

---
+++ 移动技术/backbone/spaDemo/js/base/openApi.js
@@ -0,0 +1,7 @@
+var openApi = (function() {
+    var ip = 'http://www.cnblogs.com/mvc/blog/CategoriesTags.aspx?blogApp=yexiaochai&blogId=91319&postId=3486173&_=1409208574898';
+    var openApi = {
+        tag: ip + "/mvc/blog/CategoriesTags.aspx"
+    };
+    return openApi;
+})();
