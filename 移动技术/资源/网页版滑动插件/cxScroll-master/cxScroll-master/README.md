3b74268eba9108c1aff9edb593027ade4121a0d8
haven <641136947@qq.com>
2014-09-09 16:05:16 +0800

    图片延迟加载

---
+++ 移动技术/资源/网页版滑动插件/cxScroll-master/cxScroll-master/README.md
@@ -0,0 +1,144 @@
+#jQuery cxScroll
+
+一款基于jQuery的无缝滚动插件，可自定义外观及调用参数，兼容主流浏览器。
+
+**版本：**
+
+* jQuery v1.4.4+
+* jQuery cxScroll v1.2.1
+
+**注意事项：**
+
+1. 内部会自动创建 prev 及 next 切换按钮，也可以在外部直接创建，若外部已创建或设置 prevBtn:false 及 nextBtn:false ，内部将跳过此步骤；
+2. 若（滚动的列表宽度 ≤ 父元素的宽度），则不会自动创建 prev 及 next 切换按钮，也不会自动滚动；
+3. 鼠标移动到滚动元素上时，会暂停滚动，移出后会恢复自动滚动（仅在设置 auto:true 时有效）。
+
+文档：http://code.ciaoca.com/jquery/cxscroll/
+
+示例：http://code.ciaoca.com/jquery/cxscroll/demo/
+
+##【options 参数说明】
+
+<table>
+    <tr>
+        <th width="80">名称</th>
+        <th width="80">默认值</th>
+        <th>说明</th>
+    </tr>
+    <tr>
+        <td>direction</td>
+        <td>"right"</td>
+        <td>滚动方向。可设置为："left"|"right"|"top"|"bottom"</td>
+    </tr>
+    <tr>
+        <td>easing</td>
+        <td>"swing"</td>
+        <td>缓动方式</td>
+    </tr>
+    <tr>
+        <td>step</td>
+        <td>1</td>
+        <td>滚动步长</td>
+    </tr>
+    <tr>
+        <td>accel</td>
+        <td>160</td>
+        <td>手动滚动速度(ms)</td>
+    </tr>
+    <tr>
+        <td>speed</td>
+        <td>800</td>
+        <td>自动滚动速度(ms)</td>
+    </tr>
+    <tr>
+        <td>time</td>
+        <td>4000</td>
+        <td>自动滚动间隔时间(ms)</td>
+    </tr>
+    <tr>
+        <td>auto</td>
+        <td>true</td>
+        <td>是否自动滚动</td>
+    </tr>
+    <tr>
+        <td>prevBtn</td>
+        <td>true</td>
+        <td>是否使用 prev 按钮</td>
+    </tr>
+    <tr>
+        <td>nextBtn</td>
+        <td>true</td>
+        <td>是否使用 next 按钮</td>
+    </tr>
+    <tr>
+        <td>safeLock</td>
+        <td>true</td>
+        <td>滚动时是否锁定控制按钮</td>
+    </tr>
+</table>
+
+
+##【使用方法】
+
+###CSS 样式结构
+```css
+/* 横向滚动参考样式 */
+.cxscroll{position:relative;width:660px;height:120px;padding:10px;background-color:#eee;}
+.cxscroll .box{overflow:hidden;position:relative;width:580px;height:120px;margin:0 auto;}
+.cxscroll .list{overflow:hidden;width:9999px;}
+.cxscroll .list li{float:left;width:200px;}
+.cxscroll .prev,
+.cxscroll .next{position:absolute;top:50%;margin-top:-25px;width:40px;height:50px;background-image:url(img/control.png);background-repeat:no-repeat;font:0/0 Arial;cursor:pointer;}
+.cxscroll .prev{left:5px;background-position:0 0;}
+.cxscroll .next{right:5px;background-position:-40px 0;}
+.cxscroll .prev:hover{background-position:0 -50px;}
+.cxscroll .next:hover{background-position:-40px -50px;}
+
+/* 纵向滚动参考样式 */
+.cxscroll{position:relative;width:180px;height:340px;padding:10px;background:#eee;}
+.cxscroll .box{overflow:hidden;position:relative;top:40px;height:260px;}
+.cxscroll .list{position:absolute;top:0;left:0;}
+.cxscroll .list li{height:140px;text-align:center;}
+.cxscroll .prev,
+.cxscroll .next{position:absolute;left:50%;width:120px;height:30px;margin-left:-25px;width:50px;height:40px;background-image:url(img/control.png);background-repeat:no-repeat;font:0/0 Arial;cursor:pointer;}
+.cxscroll .prev{top:5px;background-position:-80px 0;}
+.cxscroll .next{bottom:5px;background-position:-130px 0;}
+.cxscroll .prev:hover{background-position:-80px -40px;}
+.cxscroll .next:hover{background-position:-130px -40px;}
+```
+
+###DOM 结构
+```html
+<div id="element_id" class="cxscroll">
+    <div class="box">
+        <ul class="list">
+            <li></li>
+            <li></li>
+            ...
+            <li></li>
+        </ul>
+    </div>
+    <!-- 控制按钮内部会自动创建，可省略 -->
+    <a class="prev"></a>
+    <a class="next"></a>
+</div>
+```
+
+###调用 cxScroll
+```javascript
+// 直接调用
+$("#element_id").cxScroll();
+
+// 自定义参数调用
+$("#element_id").cxScroll({
+    direction:"right",
+    step:1,
+    accel:160,
+    speed:800,
+    time:4000,
+    auto:true,
+    prevBtn:true,
+    nextBtn:true,
+    safeLock:true
+});
+```
---
+++ 移动技术/资源/网页版滑动插件/cxScroll-master/cxScroll-master/js/jquery.cxscroll.js
@@ -0,0 +1,220 @@
+/*!
+ * jQuery cxScroll 1.2.1
+ * http://code.ciaoca.com/
+ * https://github.com/ciaoca/cxScroll
+ * E-mail: ciaoca@gmail.com
+ * Released under the MIT license
+ * Date: 2014-03-10
+ */
+(function($){
+	$.fn.cxScroll=function(settings){
+		if(!this.length){return};
+		settings=$.extend({},$.cxScroll.defaults,settings);
+
+		console.info("settings");
+		console.info(settings);
+
+		var obj=this;
+		var scroller={
+			lock:false,
+			dom:{}
+		};
+
+		scroller.init=function(){
+			scroller.dom.box=obj.find(".box");
+			scroller.dom.list=scroller.dom.box.find(".list");
+			scroller.dom.items=scroller.dom.list.find("li");
+			scroller.itemSum=scroller.dom.items.length;
+
+			// 没有元素或只有1个元素时，不进行滚动
+			if(scroller.itemSum<=1){return};
+
+			scroller.dom.prevBtn=obj.find(".prev");
+			scroller.dom.nextBtn=obj.find(".next");
+			scroller.itemWidth=scroller.dom.items.outerWidth();
+			scroller.itemHeight=scroller.dom.items.outerHeight();
+
+			if(settings.direction=="left"||settings.direction=="right"){
+				// 容器宽度不足时，不进行滚动
+				if(scroller.itemWidth*scroller.itemSum<=scroller.dom.box.outerWidth()){return};
+
+				scroller.prevVal="left";
+				scroller.nextVal="right";
+				scroller.moveVal=scroller.itemWidth;
+			}else{
+				// 容器高度不足时，不进行滚动
+				if(scroller.itemHeight*scroller.itemSum<=scroller.dom.box.outerHeight()){return};
+
+				scroller.prevVal="top";
+				scroller.nextVal="bottom";
+				scroller.moveVal=scroller.itemHeight;
+			};
+
+			// 元素：后补
+			scroller.dom.list.append(scroller.dom.list.html());
+
+			// 添加元素：手动操作按钮
+			if(settings.prevBtn&&!scroller.dom.prevBtn.length){
+				scroller.dom.prevBtn=$("<a></a>",{"class":"prev"}).prependTo(obj);
+			};
+			if(settings.nextBtn&&!scroller.dom.nextBtn.length){
+				scroller.dom.nextBtn=$("<a></a>",{"class":"next"}).prependTo(obj);
+			};
+
+			// 事件：鼠标移入停止，移出开始
+			if(settings.auto){
+				obj.hover(function(){
+					settings.auto=false;
+					scroller.lock=false;
+					scroller.off();
+				},function(){
+					settings.auto=true;
+					scroller.lock=false;
+					scroller.on();
+				});
+			};
+
+			scroller.bindEvents();
+			scroller.on();
+		};
+		
+		scroller.bindEvents=function(){
+			if(settings.nextBtn&&scroller.dom.prevBtn.length){
+				scroller.dom.nextBtn.bind("click",function(){
+					if(!scroller.lock){
+						scroller.goto(scroller.nextVal,settings.accel);
+					};
+				});
+			};
+			if(settings.prevBtn&&scroller.dom.prevBtn.length){
+				scroller.dom.prevBtn.bind("click",function(){
+					if(!scroller.lock){
+						scroller.goto(scroller.prevVal,settings.accel);
+					};
+				});
+			};
+		};
+
+		// 方法：开始
+		scroller.on=function(){
+			if(!settings.auto){return};
+			if(typeof(scroller.run)!=="undefined"){
+				clearTimeout(scroller.run);
+			};
+
+			scroller.run=setTimeout(function(){
+				scroller.goto(settings.direction);
+			},settings.time);
+		};
+
+		// 方法：停止
+		scroller.off=function(){
+			scroller.dom.box.stop(true);
+			if(typeof(scroller.run)!=="undefined"){
+				clearTimeout(scroller.run);
+			};
+		};
+
+		// 方法：滚动
+		scroller.goto=function(d,t){
+			scroller.off();
+			if(settings.controlLock){
+				scroller.lock=true;
+			};
+
+			var _max;	// _max	滚动的最大限度
+			var _dis;	// _dis	滚动的距离
+			var _speed=t||settings.speed;
+
+			switch(d){
+				case "left":
+				case "top":
+					_max=0;
+					if(d=="left"){
+						if(parseInt(scroller.dom.box.scrollLeft(),10)==0){
+							scroller.dom.box.scrollLeft(scroller.itemSum*scroller.moveVal);
+						};
+						_dis=scroller.dom.box.scrollLeft()-(scroller.moveVal*settings.step);
+						if(_dis%scroller.itemWidth>0){
+							_dis-=(_dis%scroller.itemWidth)-scroller.itemWidth;
+						};
+						if(_dis<_max){_dis=_max};
+						scroller.dom.box.animate({"scrollLeft":_dis},_speed,settings.easing,function(){
+							if(parseInt(scroller.dom.box.scrollLeft(),10)<=_max){
+								scroller.dom.box.scrollLeft(0);
+							};
+						});
+					}else{
+						if(parseInt(scroller.dom.box.scrollTop(),10)==0){
+							scroller.dom.box.scrollTop(scroller.itemSum*scroller.moveVal);
+						};
+						_dis=scroller.dom.box.scrollTop()-(scroller.moveVal*settings.step);
+						if(_dis%scroller.itemHeight>0){
+							_dis-=(_dis%scroller.itemHeight)-scroller.itemHeight;
+						};
+						if(_dis<_max){_dis=_max};
+						scroller.dom.box.animate({"scrollTop":_dis},_speed,settings.easing,function(){
+							if(parseInt(scroller.dom.box.scrollTop(),10)<=_max){
+								scroller.dom.box.scrollTop(0);
+							};
+						});
+					};
+					break;
+	
+				case "right":
+				case "bottom":
+					_max=scroller.itemSum*scroller.moveVal;
+					if(d=="right"){
+						_dis=scroller.dom.box.scrollLeft()+(scroller.moveVal*settings.step);
+						if(_dis%scroller.itemWidth>0){
+							_dis-=(_dis%scroller.itemWidth);
+						};
+						if(_dis>_max){_dis=_max};
+						scroller.dom.box.animate({"scrollLeft":_dis},_speed,settings.easing,function(){
+							if(parseInt(scroller.dom.box.scrollLeft(),10)>=_max){
+								scroller.dom.box.scrollLeft(0);
+							};
+						});
+					}else{
+						_dis=scroller.dom.box.scrollTop()+(scroller.moveVal*settings.step);
+						if(_dis%scroller.itemHeight>0){
+							_dis-=(_dis%scroller.itemHeight);
+						};
+						if(_dis>_max){_dis=_max};
+						scroller.dom.box.animate({"scrollTop":_dis},_speed,settings.easing,function(){
+							if(parseInt(scroller.dom.box.scrollTop(),10)>=_max){
+								scroller.dom.box.scrollTop(0);
+							};
+						});
+					};
+					break;
+				
+				// not default
+			};
+			
+			scroller.dom.box.queue(function(){
+				if(settings.controlLock){
+					scroller.lock=false;
+				};
+				scroller.on();
+				$(this).dequeue();
+			});
+		};
+
+		scroller.init();
+	};
+
+	// 默认值
+	$.cxScroll={defaults:{
+		direction:"right",	// 滚动方向
+		easing:"swing",		// 缓动方式
+		step:1,				// 滚动步长
+		accel:160,			// 手动滚动速度
+		speed:800,			// 自动滚动速度
+		time:4000,			// 自动滚动间隔时间
+		auto:true,			// 是否自动滚动
+		prevBtn:true,		// 是否使用 prev 按钮
+		nextBtn:true,		// 是否使用 next 按钮
+		safeLock:true		// 滚动时是否锁定控制按钮
+	}};
+})(jQuery);
